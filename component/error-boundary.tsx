26d052123388233fd29b6be3d9466b3847bdf8a9
Jonathan Chang <jonathan.chang@livibank.com>
2023-02-15 10:17:29 +0800

    error handling update

--- component/error-boundary.tsx
+++ component/error-boundary.tsx
@@ -1,46 +1,50 @@
 import React from "react"
+import { Title } from "livi-poc-core"
+import { IconReload } from "@tabler/icons-react"
 
 class ErrorBoundary extends React.Component {
-    constructor(props: any) {
-      super(props)
-  
-      // Define a state variable to track whether is an error or not
-      this.state = { hasError: false }
-    }
-    //@ts-ignore
-    static getDerivedStateFromError(error) {
-      // Update state so the next render will show the fallback UI
-  
-      return { hasError: true }
-    }
-    //@ts-ignore
-    componentDidCatch(error, errorInfo) {
-      // You can use your own error logging service here
-      console.log({ error, errorInfo })
-    }
-    render() {
-      // Check if the error is thrown
-      //@ts-ignore
-      if (this.state.hasError) {
-        // You can render any custom fallback UI
-        return (
-          <div className="rounded-lg shadow-lg w-full h-full border border-slate-100 bg-white p-4 flex flex-col justify-center items-center">
-            <h2 className="my-3">Oops, there is an error!</h2>
-            <button
-              type="button"
-            //   onClick={() => this.setState({ hasError: false })}
-            >
-              Try again?
-            </button>
-          </div>
-        )
-      }
-  
-      // Return children components in case of no error
-  
-      //@ts-ignore
-      return this.props.children
-    }
+  constructor(props: any) {
+    super(props)
+
+    // Define a state variable to track whether is an error or not
+    this.state = { hasError: false }
   }
-  
-  export default ErrorBoundary
+  //@ts-ignore
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI
+
+    return { hasError: true }
+  }
+  //@ts-ignore
+  componentDidCatch(error, errorInfo) {
+    // You can use your own error logging service here
+    console.log({ error, errorInfo })
+  }
+  render() {
+    // Check if the error is thrown
+    //@ts-ignore
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return (
+        <div className="rounded-lg shadow-lg w-full h-full border border-slate-100 bg-white p-4 flex flex-col justify-center items-center">
+          <Title children={'Something went wrong!'} />
+          <button
+            type="button"
+            //   onClick={() => this.setState({ hasError: false })}
+            className="bg-secondary p-4 min-w-[100px] rounded-lg flex flex-row justify-between items-center"
+          >
+            <IconReload size={20} />
+            <p className="text-primary font-bold">Reload</p>
+          </button>
+        </div>
+      )
+    }
+
+    // Return children components in case of no error
+
+    //@ts-ignore
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
